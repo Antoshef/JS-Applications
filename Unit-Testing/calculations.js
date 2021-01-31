02002f4d65a38f883f2f343026ede4e44bee65a3
Anton Stanev <antoshef21@gmail.com>
2021-01-31 20:51:24 +0200

    apps

---
+++ Unit-Testing/calculations.js
@@ -0,0 +1,14 @@
+let calculate = (a, b) => a + b;
+
+let multiply = (a, b) => a * b;
+
+let callName = (nameString) => 'Hello my name is ' + nameString;
+
+let test = (bool) => isNaN(bool);
+
+module.exports = {
+    calculate,
+    multiply,
+    callName,
+    test
+}
