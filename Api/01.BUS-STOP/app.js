e0fbc14dbd000187a91f34507630d8e9e1f3b642
Anton Stanev <antoshef21@gmail.com>
2021-02-01 20:44:10 +0200

    apps

--- Api/01.BUS-STOP/app.js
+++ Api/01.BUS-STOP/app.js
@@ -1,3 +1,20 @@
 function getInfo() {
-    console.log("TODO...");
+    
+    let input = document.getElementById('stopId').value;
+    const url = `https://judgetests.firebaseio.com/businfo/${input}.json`;
+    let stopName = document.getElementById('stopName');
+    let busesElement = document.getElementById('buses');
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            stopName.innerHTML = 'Error';
+        } else {
+            stopName.innerHTML = `<p>${data.name}</p>`;
+            let currentBuses = Object.values(data)[0];
+            for (let bus in currentBuses) {
+                busesElement.innerHTML += `<li>Bus ${bus} arrives in ${currentBuses[bus]} minutes</li>`;
+            };
+        }
+    })
 }
--- Api/02.BUS-SCHEDULE/app.js
+++ Api/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,27 @@
 function solve() {
+    
+    let currentId;
+    let nextId;
+    let url = `https://judgetests.firebaseio.com/schedule/${currentId}.json`;
+    let departedBtn = document.getElementById('depart');
+    let arrivedBtn = document.getElementById('arrive');
+    let span = document.getElementsByClassName('info')[0];
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            span.innerHTML = data.name;
+            departedBtn.disabled = true;
+            arrivedBtn.disabled = false;
+            nextId = data.next;
+            console.log(data);
+        });
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+    function depart(currentId) {
+        currentId = 'depot';
+    };
 
     function arrive() {
-        console.log('Arrive TODO...');
+        
     }
 
     return {
