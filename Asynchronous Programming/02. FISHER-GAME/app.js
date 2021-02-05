09db0f289df253abd4da6f39ab7e2e8a650cbd4a
Anton Stanev <antoshef21@gmail.com>
2021-02-05 19:42:27 +0200

    apps

--- Asynchronous Programming/02. FISHER-GAME/app.js
+++ Asynchronous Programming/02. FISHER-GAME/app.js
@@ -1,5 +1,24 @@
 function attachEvents() {
-    console.log('TODO...');
+    
+    const updateBtn = document.getElementsByClassName('update')[0];
+    const deleteBtn = document.getElementsByClassName('delete')[0];
+    const loadBtn = document.getElementsByClassName('load')[0];
+    const addBtn = document.getElementsByClassName('add')[0];
+
+    addBtn.addEventListener('click', addFisher);
+}
+
+function addFisher() {
+    let obj = {
+        angler: document.getElementsByClassName('angler')[1].value,
+        weigth: document.getElementsByClassName('weight')[1].value,
+        species: document.getElementsByClassName('species')[1].value,
+        location: document.getElementsByClassName('location')[1].value,
+        bait: document.getElementsByClassName('bait')[1].value,
+        captureTime: document.getElementsByClassName('captureTime')[1].value,
+    }
+
+    fetch('https://fisher-game.firebaseio.com/catches.json', {method: 'POST', body: JSON.stringify(obj)});
 }
 
 attachEvents();
