01d2f2278d868d1138f88fe25978ced82f170d23
Anton Stanev <antoshef21@gmail.com>
2021-02-05 21:23:45 +0200

    apps

--- Asynchronous Programming/02. FISHER-GAME/app.js
+++ Asynchronous Programming/02. FISHER-GAME/app.js
@@ -6,6 +6,7 @@ function attachEvents() {
     const addBtn = document.getElementsByClassName('add')[0];
 
     addBtn.addEventListener('click', addFisher);
+    loadBtn.addEventListener('click', loadData);
 }
 
 function addFisher() {
@@ -18,7 +19,31 @@ function addFisher() {
         captureTime: document.getElementsByClassName('captureTime')[1].value,
     }
 
-    fetch('https://fisher-game.firebaseio.com/catches.json', {method: 'POST', body: JSON.stringify(obj)});
+    fetch('https://fisher-game.firebaseio.com/catches.json', {
+    method: 'POST', 
+    body: JSON.stringify(obj)
+    });
+}
+
+function loadData() {
+    let fieldset = document.getElementById('main');
+
+    fetch('https://fisher-game.firebaseio.com/catches.json')
+    .then(res => res.json())
+    .then(data => {
+        let ulElement = document.createElement('ul');
+        Object.entries(data).map(currentCatch => {
+            let result = `ID: ${currentCatch[0]}, `;
+            let li = document.createElement('li');
+
+            Object.entries(currentCatch[1]).map(fish => {
+                result += `${fish[0]}: ${fish[1]}, `;
+            })
+            li.innerHTML = result;
+            ulElement.appendChild(li);
+        })
+        fieldset.appendChild(ulElement);
+    })
 }
 
 attachEvents();
