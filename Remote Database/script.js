5795eb2996b77a448344ff187355bc2ccf2dbdcb
Anton Stanev <antoshef21@gmail.com>
2021-02-06 21:04:29 +0200

    apps

---
+++ Remote Database/script.js
@@ -0,0 +1,10 @@
+
+let artist = document.querySelector('artist');
+let title = document.querySelector('title');
+let rating = document.querySelector('rating');
+let list = document.querySelector('favorite-songs');
+
+function submitSong() {
+    let liElement = document.createElement("li", "asd", "qwer");
+    list.appendChild(liElement);
+}
